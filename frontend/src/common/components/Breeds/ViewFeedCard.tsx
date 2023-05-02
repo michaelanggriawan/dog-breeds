51cfbeaf6f0ee6941dc6206973aa1bc7a4bb0789
Michael Anggriawan <michaelanggriawan941@gmail.com>
2023-05-03 06:40:15 +0700

    feat: add like icon

--- frontend/src/common/components/Breeds/ViewFeedCard.tsx
+++ frontend/src/common/components/Breeds/ViewFeedCard.tsx
@@ -1,4 +1,12 @@
-import { Card, CardHeader, CardMedia } from '@mui/material';
+import { useState } from 'react';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import {
+  Card,
+  CardActions,
+  CardHeader,
+  CardMedia,
+  IconButton,
+} from '@mui/material';
 
 type SelectedBreedCardProps = {
   image: string;
@@ -6,10 +14,19 @@ type SelectedBreedCardProps = {
 };
 
 export default function ViewFeedCard({ image, breed }: SelectedBreedCardProps) {
+  const [isLike, setIsLike] = useState(false);
   return (
     <Card sx={{ width: 345 }}>
       <CardHeader title={breed} />
       <CardMedia component="img" height="194" image={image} alt={breed} />
+      <CardActions>
+        <IconButton
+          aria-label="add to favorites"
+          onClick={() => setIsLike(!isLike)}
+        >
+          <FavoriteIcon color={isLike ? 'secondary' : 'action'} />
+        </IconButton>
+      </CardActions>
     </Card>
   );
 }
